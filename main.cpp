96d8ebc0f13ba6548f1a3bc0986733bd3efa4d50
user <irbis1516@gamil.com>
2024-08-02 22:19:39 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
 #include <string>
 #include "Comparator.h"
 
+void lower(char* str){
+    while (*str) {
+        *str = std::tolower(*str);
+        *str = *str;
+        str++;
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 3) {
         std::cout << "Not enougth args" << std::endl;
         exit(1);
     }
-
+    lower(argv[1]); lower(argv[2]);
+    std::cout << argv[1] << ' ' << argv[2];
     Comparator c(argv[1], argv[2]);
     c.compare();
     c.save();
